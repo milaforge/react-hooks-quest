c405ccf0aefcc642404df10413643bfe8d6e82d3
mf <milaforge@proton.me>
2026-08-10 18:39:41 +0330

    feat(status): reload on focus

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
+
 import { loadPlayer } from "../storage/player";
 import { HUD } from "../components/HUD";
 import { MissionList } from "../components/MissionList";
 
 export default function Home() {
-  const player = loadPlayer();
+  const [player, setPlayer] = useState(loadPlayer());
+
+  useEffect(() => {
+    setPlayer(loadPlayer());
+  }, []);
+
+  useEffect(() => {
+    const refresh = () => setPlayer(loadPlayer());
+    window.addEventListener("focus", refresh);
+    return () => window.removeEventListener("focus", refresh);
+  }, []);
 
   return (
     <main>

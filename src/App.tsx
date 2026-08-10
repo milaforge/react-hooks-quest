33bb5b69af958844b6a110c3dc19f00fe3c10dc9
mf <milaforge@proton.me>
2026-08-10 13:01:42 +0330

    feat(routing): proper routing for github pages and client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+
 import Home from "./pages/Home";
+import MissionPage from "./pages/MissionPage";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter basename="/react-hooks-quest">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mission/:id" element={<MissionPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
